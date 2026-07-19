9a3c6d02f7b89a988d371999e3b585f2461fab25
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2026-07-19 22:44:54 +0900

    feat:GetMapping(/)を追加

--- src/main/java/net/westdaiyo/binarytrainer/controller/QuestionController.java
+++ src/main/java/net/westdaiyo/binarytrainer/controller/QuestionController.java
@@ -1,6 +1,9 @@
 package net.westdaiyo.binarytrainer.controller;
 import org.springframework.stereotype.Controller;
 import net.westdaiyo.binarytrainer.service.QuestionService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
 
 @Controller
 public class QuestionController {
@@ -9,4 +12,10 @@ public class QuestionController {
     public QuestionController(QuestionService questionService) {
         this.questionService = questionService;
     }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+    
 }
