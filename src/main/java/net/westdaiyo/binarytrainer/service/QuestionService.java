5cb53d3ac11f7053ff912e3ded386b90137e2c51
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2026-07-19 12:19:00 +0900

    -m "色々修正and追加"　\ -m "modelフォルダにConversionType.javaを作成。conversitonTypeをenum型に。" "modelフォルダのQestion.javaのフィールド変数をConversionType型に変更" "ServiceフォルダのQestionService.javaにて、ConversionTypeの値によって問題作成ロジックをswitchで分岐"

--- src/main/java/net/westdaiyo/binarytrainer/service/QuestionService.java
+++ src/main/java/net/westdaiyo/binarytrainer/service/QuestionService.java
@@ -1,9 +1,32 @@
 package net.westdaiyo.binarytrainer.service;
+import net.westdaiyo.binarytrainer.model.ConversionType;
 import net.westdaiyo.binarytrainer.model.Question;
+import java.util.Random;
+import org.springframework.stereotype.Service;
 
+@Service
 public class QuestionService {
 
-    public Question createQuestion() {
-        return new Question("25","11001","10→2");
+    private final Random random = new Random();
+
+    public Question createQuestion(ConversionType conversionType) {
+        switch (conversionType) {
+            case DECIMAL_TO_BINARY:
+                return createDecimalToBinaryQuestion();        
+            default:
+                throw new UnsupportedOperationException(
+                    "存在しない変換形式です : " + conversionType 
+                );  //暫定処置
+        }
+    }
+
+    // 10進数から2進数への変換問題作成
+    private Question createDecimalToBinaryQuestion() {
+        int number = random.nextInt(256); //0~255までのランダムな10進数を作成
+
+        String question = String.valueOf(number);
+        String answer = Integer.toBinaryString(number);
+
+        return new Question(question, answer, ConversionType.DECIMAL_TO_BINARY);
     }
 }
